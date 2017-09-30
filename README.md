a9396e4b9a0d98c5315aea26689a2c58cd70a97f
GhostComputing <zyylsxm@gmail.com>
2017-09-30 16:14:22 +0800

    first commmit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Overview
+
+It's a test project of [googletest](https://github.com/google/googletest).
+
+Nothing but test the API and features.
+
+## How to Build and Run
+
+- Builld googletest
+
+        $ cd googletest-release-1.8.0
+        $ mkdir build && cd build
+        $ mkdir install && cmake -DCMAKE_INSTALL_PREFIX=${PWD}/install ..
+        $ make -j`nproc` && make install
+   
+  See `deps/gtest`, it's pre-built version.
+
+- Build the test code
+        
+        $ cd try-googletest
+        $ mkdir build && cd build
+        $ cmake ..
+        $ make -j`nproc`
+        
+- Run 
+
+        $ cd try-googletest
+        $ ./build/sample1_unittest
---
+++ src/sample1_unittest.cc
@@ -0,0 +1,72 @@
+#include <limits.h>
+#include "sample1.h"
+#include "gtest/gtest.h"
+
+// This sample shows how to write a simple unit test for a function,
+// using Google C++ testing framework.
+//
+// Writing a unit test using Google C++ testing framework is easy as 1-2-3:
+
+// Step 1. Include necessary header files such that the stuff your
+// test logic needs is declared.
+//
+// Don't forget gtest.h, which declares the testing framework.
+
+namespace {
+
+// Step 2. Use the TEST macro to define your tests.
+//
+// TEST has two parameters: the test case name and the test name.
+// After using the macro, you should define your test logic between a
+// pair of braces. You can use a bunch of macros to indicate the success
+// or failure of a test. EXPECT_TRUE and EXPECT_EQ are example of sunch
+// macros. For a complete list, see gtest.h
+
+TEST(FactorialTest, Negative) {
+    // This test is named "Negative", and belongs to the "FactorialTest"
+    // test case
+    EXPECT_EQ(1, Factorial(-5));
+    EXPECT_EQ(1, Factorial(-1));
+    EXPECT_GT(Factorial(-10), 0);
+}
+
+TEST(FactorialTest, Positive) {
+    EXPECT_EQ(1, Factorial(1));
+    EXPECT_EQ(2, Factorial(2));
+    EXPECT_EQ(6, Factorial(3));
+    EXPECT_EQ(40320, Factorial(8));
+}
+
+// Tests IsPrime()
+TEST(IsPrimeTest, Negative) {
+    // This test belongs to the IsPrimeTest test case.
+    EXPECT_FALSE(IsPrime(-1));
+    EXPECT_FALSE(IsPrime(-2));
+    EXPECT_FALSE(IsPrime(INT_MIN));
+}
+
+// Tests some trivial cases.
+TEST(IsPrimeTest, Trivial) {
+    EXPECT_FALSE(IsPrime(0));
+    EXPECT_FALSE(IsPrime(1));
+    EXPECT_TRUE(IsPrime(2));
+    EXPECT_TRUE(IsPrime(3));
+}
+
+// Tests positive input
+TEST(IsPrimeTest, Positive) {
+    EXPECT_FALSE(IsPrime(4));
+    EXPECT_TRUE(IsPrime(5));
+    EXPECT_FALSE(IsPrime(6));
+    EXPECT_TRUE(IsPrime(23));
+}
+} // namespace
+
+// Step 3. Call RUN_ALL_TESTS() in main().
+//
+// We do this by linking in src/gtest_main.cc file, which consists of
+// a main() function which calls RUN_ALL_TESTS() for us.
+//
+// This runs all the tests you've defined, prints the result, and
+// returns 0 if successful, or 1 otherwise.
+//
